0ad2d83c960c3ef3642d7d94548941440ea3fc20
A-lineed <aline.edvania.franca@outlook.com>
2025-05-05 17:11:28 -0300

    Corrigindo teste flaky com o uso do intercept

--- cypress/e2e/flaky-test/sample.cy.js
+++ cypress/e2e/flaky-test/sample.cy.js
@@ -1,25 +1,26 @@
 import { faker } from '@faker-js/faker'
 
-describe('Flaky tests bad practice', () => {
+describe.only('Flaky tests bad practice', () => {
   beforeEach(() => {
+
+    cy.intercept(
+      'GET',
+      '**/search**'
+    ).as('getStories')
+
     cy.visit('https://wlsf82-hacker-stories.web.app')
 
-    cy.contains('p','Loading ...')
-      .should('be.visible')
-    cy.contains('p','Loading ...')
-      .should('not.exist')
+    cy.wait('@getStories')
+
   })
 
   Cypress._.times(10, () => {
-    it('shows a max of 5 buttons for the last searched terms', () => {
+    it.only('shows a max of 5 buttons for the last searched terms', () => {
       Cypress._.times(6, () => {
         cy.search(faker.random.word())
       })
 
-      cy.contains('p','Loading ...')
-        .should('be.visible')
-      cy.contains('p','Loading ...')
-        .should('not.exist')
+      cy.wait('@getStories')
 
       cy.get('.last-searches button')
         .should('have.length', 5)
